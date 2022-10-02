36771dc15c244834c9653e20d038c21512adb037
it9gamelog <40131161+it9gamelog@users.noreply.github.com>
2022-10-02 22:19:44 +0900

    Add lang, locale

--- docs/solarterm/en/gen.sh
+++ docs/solarterm/en/gen.sh
@@ -3,5 +3,7 @@
 for i in `seq 1900 2049`; do echo "---
 title: Dates of $i Zodiac Signs and Solar Terms
 description: The dates and times of the tropical zodiac signs, widely used in western astrology, and solar terms of year $i
+lang: en-US
+locale: en_US
 ---
 {% include_relative _solarterm_details.html year=\"$i\" %}" > $i.md; done
